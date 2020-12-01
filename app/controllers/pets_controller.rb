f4949632e0dbee2fbaaf646b84ed6ca0cf3243b3
Hanh Solo <minhhanhnyc@gmail.com>
2020-12-01 15:42:28 -0800

    pet controller index tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,13 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.order(:name)
-    render json: { ok: "YEAH" }, status: :ok
+    # @pets = Pet.order(:name)
+    # render json: { ok: "YEAH" }, status: :ok
+    pets = Pet.order(:name)
+    render json: pets.as_json(only: [:id, :name, :age, :owner, :species]),
+                              status: :ok
+  end
+
+  def show
+    
   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -3,9 +3,43 @@ require "test_helper"
 describe PetsController do
   it "must get index" do
     get pets_path
-
+    # making sure it returning json
     must_respond_with :success
     expect(response.header['Content-Type']).must_include 'json'
   end
+
+  it "will return all the proper fileds for a list of pets" do 
+    pet_fields = ["id", "name", "age", "owner", "species"].sort
+
+    # Act
+    get pets_path
+
+    # Get the body of the response as an array or hash
+    
+    body = JSON.parse(response.body)
+
+    # Assert
+    expect(body).must_be_instance_of Array
+
+    body.each do |pet|
+      expect(pet).must_be_instance_of Hash 
+      expect(pet.keys.sort).must_equal pet_fields 
+    end
+  end
+
+  it "returns an empty array if no pet exits" do
+    # Arrange
+    Pet.destroy_all
+
+    # Act
+    get pets_path
+    body = JSON.parse(response.body)
+
+    # Assert
+    expect(body).must_be_instance_of Array
+    expect(body).must_equal []
+    expect(body.length).must_equal 0
+  end
+
 
 end
