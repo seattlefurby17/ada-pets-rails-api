0bbe29fbc11aa00e7e20be5325f881fbeaf1868f
Hanh Solo <minhhanhnyc@gmail.com>
2020-12-01 21:29:26 -0800

    Added create method, updated route and test

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,6 +8,33 @@ class PetsController < ApplicationController
   end
 
   def show
-    
+    pet = Pet.find_by(id: params[:id])
+
+    if pet.nil?
+      render json: {
+        ok: false,
+        message: 'No pet found'
+      }, status: :not_found
+      return
+    end
+
+    render json: pet.as_json(only: [:id, :name, :age, :owner, :species]),
+                            status: :ok
   end
+
+  def create
+    pet = Pet.new(pet_params)
+    if pet.save
+      render json: pet.as_json, status: :created
+    else
+      
+    end
+  end
+
+  private
+
+  def pet_params
+    return params.require(:pet).permit(:name, :age, :owner, :species)
+  end
+
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,45 +1,103 @@
 require "test_helper"
 
 describe PetsController do
-  it "must get index" do
-    get pets_path
-    # making sure it returning json
-    must_respond_with :success
-    expect(response.header['Content-Type']).must_include 'json'
-  end
+  describe 'index' do 
+    PET_FIELDS = ["id", "name", "age", "owner", "species"].sort
+    it "must get index" do
+      get pets_path
+      # making sure it returning json
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+    end
 
-  it "will return all the proper fileds for a list of pets" do 
-    pet_fields = ["id", "name", "age", "owner", "species"].sort
+    it "will return all the proper fileds for a list of pets" do 
 
-    # Act
-    get pets_path
+      # Act
+      get pets_path
 
-    # Get the body of the response as an array or hash
-    
-    body = JSON.parse(response.body)
+      # Get the body of the response as an array or hash
+      body = JSON.parse(response.body)
 
-    # Assert
-    expect(body).must_be_instance_of Array
+      # Assert
+      expect(body).must_be_instance_of Array
 
-    body.each do |pet|
-      expect(pet).must_be_instance_of Hash 
-      expect(pet.keys.sort).must_equal pet_fields 
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash 
+        expect(pet.keys.sort).must_equal PET_FIELDS
+      end
+    end
+
+    it "returns an empty array if no pet exits" do
+      # Arrange
+      Pet.destroy_all
+
+      # Act
+      get pets_path
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+      expect(body.length).must_equal 0
+    end
+
+    describe "show" do
+      # Nominal 
+      it "will return a hash with the proper fields for an existing pet" do
+        # Arrange
+        pet = pets(:luna)
+
+        # Act
+        get pet_path(pet.id)
+        
+        # Assert
+        must_respond_with :ok
+
+        # Get the body of the response as an array or hash
+        body = JSON.parse(response.body)
+
+        #making sure it returning json
+        must_respond_with :success
+        expect(response.header['Content-Type']).must_include 'json'
+
+        expect(body).must_be_instance_of Hash
+        expect(body.keys.sort).must_equal PET_FIELDS
+
+      end
+
+      #Edge Case
+      it "will retrun a 404 request with json for a non-existent pet" do
+        get pet_path(-1)
+
+        must_respond_with :not_found
+        body = JSON.parse(response.body)
+        expect(body).must_be_instance_of Hash
+        expect(body['ok']).must_equal false
+        expect(body['message']).must_equal 'No pet found'
+      end
+    end
+  end 
+      
+  describe 'create' do
+    it 'can create a new pet' do
+      # arrange
+      pet_params = {
+        pet: {
+          name: 'kobi',
+          age: 3, 
+          owner: 'solo', 
+          species: 'feline'
+        }
+      }
+      # act and assert 
+      # pet was addedd to db
+      expect{
+        post pets_path, params: pet_params
+      }.must_differ "Pet.count", 1
+      
+      # response code 201 was created
+      must_respond_with :created
     end
   end
-
-  it "returns an empty array if no pet exits" do
-    # Arrange
-    Pet.destroy_all
-
-    # Act
-    get pets_path
-    body = JSON.parse(response.body)
-
-    # Assert
-    expect(body).must_be_instance_of Array
-    expect(body).must_equal []
-    expect(body.length).must_equal 0
-  end
-
 
 end
