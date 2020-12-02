ae66cad85692e9ad6ca3223123e3abffcc87d41e
Hanh Solo <minhhanhnyc@gmail.com>
2020-12-01 22:20:06 -0800

    dry up index testing  with check_response method

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -3,11 +3,21 @@ require "test_helper"
 describe PetsController do
   describe 'index' do 
     PET_FIELDS = ["id", "name", "age", "owner", "species"].sort
+
+    def check_response(expected_type:, expected_status: :success)
+      must_respond_with expected_status
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+       expect(body).must_be_instance_of expected_type
+       return body
+    end
+
     it "must get index" do
       get pets_path
+      check_response(expected_type: Array)
       # making sure it returning json
-      must_respond_with :success
-      expect(response.header['Content-Type']).must_include 'json'
+      # must_respond_with :success
+      # expect(response.header['Content-Type']).must_include 'json'
     end
 
     it "will return all the proper fileds for a list of pets" do 
@@ -16,10 +26,10 @@ describe PetsController do
       get pets_path
 
       # Get the body of the response as an array or hash
-      body = JSON.parse(response.body)
-
-      # Assert
-      expect(body).must_be_instance_of Array
+      # body = JSON.parse(response.body)
+      body = check_response(expected_type: Array)
+      # # Assert
+      # expect(body).must_be_instance_of Array
 
       body.each do |pet|
         expect(pet).must_be_instance_of Hash 
@@ -33,10 +43,11 @@ describe PetsController do
 
       # Act
       get pets_path
-      body = JSON.parse(response.body)
+      # body = JSON.parse(response.body)
 
-      # Assert
-      expect(body).must_be_instance_of Array
+      # # Assert
+      # expect(body).must_be_instance_of Array
+      body = check_response(expected_type: Array)
       expect(body).must_equal []
       expect(body.length).must_equal 0
     end
@@ -51,16 +62,17 @@ describe PetsController do
         get pet_path(pet.id)
         
         # Assert
-        must_respond_with :ok
+        # must_respond_with :ok
+        check_response(expected_type: Hash)
 
         # Get the body of the response as an array or hash
-        body = JSON.parse(response.body)
+        # body = JSON.parse(response.body)
+        body = check_response(expected_type: Hash)
+        # #making sure it returning json
+        # must_respond_with :success
+        # expect(response.header['Content-Type']).must_include 'json'
 
-        #making sure it returning json
-        must_respond_with :success
-        expect(response.header['Content-Type']).must_include 'json'
-
-        expect(body).must_be_instance_of Hash
+        # expect(body).must_be_instance_of Hash
         expect(body.keys.sort).must_equal PET_FIELDS
 
       end
@@ -68,12 +80,15 @@ describe PetsController do
       #Edge Case
       it "will retrun a 404 request with json for a non-existent pet" do
         get pet_path(-1)
-
+        # check_response(expected_type: Hash)
+        # body = check_response(expected_type: Hash)
+        
         must_respond_with :not_found
         body = JSON.parse(response.body)
         expect(body).must_be_instance_of Hash
         expect(body['ok']).must_equal false
         expect(body['message']).must_equal 'No pet found'
+        
       end
     end
   end 
@@ -110,6 +125,7 @@ describe PetsController do
       }.wont_change "Pet.count"
 
       must_respond_with :bad_request 
+      # check_response(expected_type: Hash)
 
       expect(response.header['Content-Type']).must_include 'json'
       body = JSON.parse(response.body)
